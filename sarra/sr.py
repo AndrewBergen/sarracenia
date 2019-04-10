503f092f2602d31081cebd7ef0388afd01b93b6e
Benoit Lapointe <benoit.lapointe@canada.ca>
2019-04-10 14:25:42 -0400

    Added current work documentation and analysis on sarracenia.
    + Also added a python unit test demo class (not used for now)

--- sarra/sr.py
+++ sarra/sr.py
@@ -34,167 +34,173 @@
 #
 import logging, os, os.path, shutil, sys, time
 
-try :    
-         from sr_config          import *
-         from sr_poll            import *
-         from sr_post            import *
-         from sr_report          import *
-         from sr_sarra           import *
-         from sr_sender          import *
-         from sr_shovel          import *
-         from sr_subscribe       import *
-         from sr_watch           import *
-         from sr_winnow          import *
-except : 
-         from sarra.sr_config    import *
-         from sarra.sr_poll      import *
-         from sarra.sr_post      import *
-         from sarra.sr_report    import *
-         from sarra.sr_sarra     import *
-         from sarra.sr_sender    import *
-         from sarra.sr_shovel    import *
-         from sarra.sr_subscribe import *
-         from sarra.sr_watch     import *
-         from sarra.sr_winnow    import *
+try:
+    from sr_config import *
+    from sr_poll import *
+    from sr_post import *
+    from sr_report import *
+    from sr_sarra import *
+    from sr_sender import *
+    from sr_shovel import *
+    from sr_subscribe import *
+    from sr_watch import *
+    from sr_winnow import *
+except:
+    from sarra.sr_config import *
+    from sarra.sr_poll import *
+    from sarra.sr_post import *
+    from sarra.sr_report import *
+    from sarra.sr_sarra import *
+    from sarra.sr_sender import *
+    from sarra.sr_shovel import *
+    from sarra.sr_subscribe import *
+    from sarra.sr_watch import *
+    from sarra.sr_winnow import *
 
 
-# Uses sr_instances to get a good logger
-cfg = sr_instances()
+def instantiate(cfg, pgm, confname, action):
+    """ Instantiate each program  with its configuration file
 
-# instantiate each program  with its configuration file
-# and invoke action if one of cleanup,declare,setup
+    And invoke action if one of cleanup,declare,setup
 
-def instantiate(dirconf,pgm,confname,action):
-
+    :param cfg:
+    :param pgm:
+    :param confname:
+    :param action:
+    :return:
+    """
     # c stuff always requiere to spawn a call
 
-    if pgm in ['audit','cpost','cpump'] :
-       # try to avoid error code while running sanity
-       if action == 'sanity' : return
-       cfg.logger.debug("%s %s %s" % ("sr_" + pgm,action,confname))
-       cfg.run_command([ "sr_" + pgm, action, confname])
-       return
+    if pgm in ['audit', 'cpost', 'cpump']:
+        # try to avoid error code while running sanity
+        if action == 'sanity': return
+        cfg.logger.debug("%s %s %s" % ("sr_" + pgm, action, confname))
+        cfg.run_command(["sr_" + pgm, action, confname])
+        return
 
-    #print(dirconf,pgm,confname,action)
-
-    config      = re.sub(r'(\.conf)','',confname)
-    orig        = sys.argv[0]
+    config = re.sub(r'(\.conf)', '', confname)
+    orig = sys.argv[0]
 
     sys.argv[0] = 'sr_' + pgm
 
     try:
-            inst  = None
-            cfg.logger.debug("inst %s %s %s" % (pgm,config,action))
-            if    pgm == 'poll':      inst = sr_poll     (config,[action])
-            elif  pgm == 'post':      inst = sr_post     (config,[action])
-            elif  pgm == 'sarra':     inst = sr_sarra    (config,[action])
-            elif  pgm == 'sender':    inst = sr_sender   (config,[action])
-            elif  pgm == 'shovel':    inst = sr_shovel   (config,[action])
-            elif  pgm == 'subscribe': inst = sr_subscribe(config,[action])
-            elif  pgm == 'watch':     inst = sr_watch    (config,[action])
-            elif  pgm == 'winnow':    inst = sr_winnow   (config,[action])
-            elif  pgm == 'report':    inst = sr_report   (config,[action])
-            elif  pgm == 'audit':     inst = sr_audit    (config,[action])
-            else: 
-                  cfg.logger.error("code not configured for process type sr_%s" % pgm)
-                  sys.exit(1)
-
-            if    action == 'cleanup': inst.exec_action('cleanup',False)
-            elif  action == 'declare': inst.exec_action('declare',False)
-            elif  action == 'setup':   inst.exec_action('setup',  False)
-
-            elif  action == 'remove':  inst.exec_action('remove',False)
-
-            sys.argv[0] = orig
-
-    except:
-            cfg.logger.error("could not instantiate and run sr_%s %s %s" % (pgm,action,confname))
+        inst = None
+        cfg.logger.debug("inst %s %s %s" % (pgm, config, action))
+        if pgm == 'poll':
+            inst = sr_poll(config, [action])
+        elif pgm == 'post':
+            inst = sr_post(config, [action])
+        elif pgm == 'sarra':
+            inst = sr_sarra(config, [action])
+        elif pgm == 'sender':
+            inst = sr_sender(config, [action])
+        elif pgm == 'shovel':
+            inst = sr_shovel(config, [action])
+        elif pgm == 'subscribe':
+            inst = sr_subscribe(config, [action])
+        elif pgm == 'watch':
+            inst = sr_watch(config, [action])
+        elif pgm == 'winnow':
+            inst = sr_winnow(config, [action])
+        elif pgm == 'report':
+            inst = sr_report(config, [action])
+        elif pgm == 'audit':
+            inst = sr_audit(config, [action])
+        else:
+            cfg.logger.error("code not configured for process type sr_%s" % pgm)
             sys.exit(1)
 
-      
+        inst.logger = cfg.logger
 
-# invoke each program with its action and configuration file
+        if action == 'cleanup':
+            inst.exec_action('cleanup', False)
+        elif action == 'declare':
+            inst.exec_action('declare', False)
+        elif action == 'setup':
+            inst.exec_action('setup', False)
 
-def invoke(dirconf,pgm,confname,action):
+        elif action == 'remove':
+            inst.exec_action('remove', False)
 
+        sys.argv[0] = orig
+
+    except:
+        cfg.logger.error("could not instantiate and run sr_%s %s %s" % (pgm, action, confname))
+        cfg.logger.debug('Exception details: ', exc_info=True)
+        sys.exit(1)
+
+
+def invoke(cfg, pgm, confname, action):
+    """ Invoke each program with its action and configuration file
+
+    :param cfg:
+    :param pgm:
+    :param confname:
+    :param action:
+    :return:
+    """
     program = 'sr_' + pgm
-    config  = re.sub(r'(\.conf)','',confname)
+    config = re.sub(r'(\.conf)', '', confname)
 
     # c does not implement action sanity yet
     cfg.logger.info("action %s" % action)
 
-    try :
-             # anything but sr_post
-             if program != 'sr_post' :
-                cfg.logger.debug("%s %s %s" % (program,action,config))
-                cfg.run_command([program,action,config])
-                return
-
-             # sr_post needs -c with absolute confpath
-
-             confpath = dirconf + os.sep + pgm + os.sep + confname
-             sleeps=False
-
-             if ( action == 'status' ) :
-                 f=open(confpath,'r')
-                 for li in f.readlines():
-                     l = li.split()
-                     if len(l) < 2 :
+    try:
+        # anything but sr_post
+        if program != 'sr_post':
+            cfg.logger.debug("%s %s %s" % (program, action, config))
+            cfg.run_command([program, action, config])
+        else:
+            # sr_post needs -c with absolute confpath
+            confpath = cfg.user_config_dir + os.sep + pgm + os.sep + confname
+            sleeps = False
+            if (action == 'status'):
+                f = open(confpath, 'r')
+                for li in f.readlines():
+                    l = li.split()
+                    if len(l) < 2:
                         continue
 
-                     if l[0] == 'sleep' :
-                        if  float(l[1]) > 0:
-                           sleeps=True
-                 f.close()
-
-             if not sleeps:
-                 return
-
-             post = sr_post(confpath)
-
-             cfg.logger.debug("INVOKE %s %s %s %s" % (program,'-c',confpath,action))
-             cfg.run_command([program,'-c',confpath,action])
-             return
-
-    except :
-             cfg.logger.error("Invoke failed")
-             cfg.logger.debug('Exception details: ', exc_info=True)
+                    if l[0] == 'sleep':
+                        if float(l[1]) > 0:
+                            sleeps = True
+                f.close()
+            if not sleeps:
+                return
+            post = sr_post(confpath)
+            cfg.logger.debug("INVOKE %s %s %s %s" % (program, '-c', confpath, action))
+            cfg.run_command([program, '-c', confpath, action])
+    except:
+        cfg.logger.error("Invoke failed")
+        cfg.logger.debug('Exception details: ', exc_info=True)
 
 
-# check number of config files
+def scandir(cfg, pgm, action):
+    """ Recursive scan of ~/.config/sarra/* , invoking process according to
 
-def nbr_config(dirconf):
-    n = 0
+    the process named from the parent directory
 
-    if not os.path.isdir(dirconf)       : return 0
+    :param cfg:
+    :param pgm:
+    :param action:
+    :return:
+    """
+    config_path = cfg.user_config_dir + os.sep + pgm
+    configpath_exist = os.path.isdir(config_path) and len(os.listdir(config_path)) != 0
 
-    for confname in os.listdir(dirconf) :
-        if not '.conf' in confname      : continue
-        n += 1
- 
-    return n
-
-# recursive scan of ~/.config/sarra/* , invoking process according to
-# the process named from the parent directory
-
-def scandir(dirconf,pgm,action):
-
-    path = dirconf + os.sep + pgm
-
-    if not os.path.isdir(path) or len(os.listdir(path)) == 0 : 
-       if pgm == 'audit' :
-          cfg.logger.info("%s %s" % (pgm,action))
-          cfg.run_command(['sr_'+pgm,action])
-       return
-
-    for confname in os.listdir(path) :
-        if len(confname) < 5                 : continue
-        if not '.conf' in confname[-5:]      : continue
-        cfg.logger.info("%s %s %s" % (pgm,action,confname))
-        if action in ['cleanup','declare','setup']:
-              instantiate(dirconf,pgm,confname,action)
-        else:
-              invoke(dirconf,pgm,confname,action)
+    if (not configpath_exist) and pgm == 'audit':
+        cfg.logger.info("sr_%s %s" % (pgm, action))
+        cfg.run_command(['sr_' + pgm, action])
+    elif configpath_exist:
+        for confname in os.listdir(config_path):
+            if len(confname) >= 5 and '.conf' in confname[-5:]:
+                cfg.logger.info("%s %s %s" % (pgm, action, confname))
+                if action in ['cleanup', 'declare', 'setup']:
+                    instantiate(cfg, pgm, confname, action)
+                else:
+                    invoke(cfg, pgm, confname, action)
+    else:
+        cfg.logger.debug('No configuration found for sr_%s in %s' % (pgm, cfg.user_config_dir))
 
 
 # ===================================
@@ -202,63 +208,64 @@ def scandir(dirconf,pgm,action):
 # ===================================
 
 def main():
+    # Uses sr_instances to get a good logger
+    cfg = sr_instances(args=sys.argv)
 
     # actions supported
-    actions_supported = ['start', 'stop', 'status', 'sanity', 'restart', 'reload', 'cleanup', 'declare', 'setup', 'remove']
+    actions_supported = ['start', 'stop', 'status', 'sanity', 'restart', 'reload', 'remove',
+                         'cleanup', 'declare', 'setup']
 
     # actions extended (actions on config)
-    actions_supported.extend( ['list'] )
-
-    actstr = str(actions_supported)
-    actstr = actstr.replace(", ","|")
+    actions_supported.extend(['list'])
 
     # validate action
-    if len(sys.argv) == 1 or sys.argv[1] not in actions_supported :
-       print("USAGE: %s %s (version: %s) " % (sys.argv[0],actstr,sarra.__version__) )
-       sys.exit(1)
+    if len(sys.argv) == 1 or sys.argv[1] not in actions_supported:
+        print("USAGE: %s %s (version: %s) " % (sys.argv[0], '|'.join(actions_supported), sarra.__version__))
+        sys.exit(1)
+    elif len(sys.argv) == 2 or len(sys.argv) == 3 and sys.argv[2] == '-debug':
+        action = sys.argv[1]
+        config = None
+    elif len(sys.argv) > 3 and sys.argv[2] != '-debug':
+        action = sys.argv[1]
+        config = sys.argv[2]
+    else:
+        action = sys.argv[1]
+        config = sys.argv[3]
 
-    action = sys.argv[1]
-    config = None
+    # Main switch
+    if action == 'list' and config and config == 'plugins':
+        # List plugins
+        cfg.print_configdir("packaged plugins", cfg.package_dir + os.sep + 'plugins')
+        cfg.print_configdir("user plugins", cfg.user_config_dir + os.sep + 'plugins')
+    elif action == 'list' and config:
+        # Print config file content
+        result = cfg.find_conf_file(config)
+        if not result:
+            print("no file named %s found in all sarra configs" % config)
+            sys.exit(1)
+        cfg.list_file(result)
+    elif action == 'list':
+        # List all configs
+        for pgm in sorted(cfg.programs):
+            cfg.print_configdir("configuration examples", cfg.package_dir + os.sep + 'examples' + os.sep + pgm)
+        cfg.print_configdir("general", cfg.user_config_dir)
+        for pgm in sorted(cfg.programs):
+            cfg.print_configdir("user configurations", cfg.user_config_dir + os.sep + pgm)
+    else:
+        # Execute action on all configured processes
 
-    if len(sys.argv) == 3 : config = sys.argv[2]
+        # Init logger here
+        cfg.build_instance(1)
+        cfg.setlog()
 
-    # action list
+        # loop on all possible programs add audit at beginning
+        for pgm in ['audit'] + cfg.programs:
+            scandir(cfg, pgm, action)
 
-    if action == 'list' :
-       if config :
-           if config == 'plugins' :
-               cfg.print_configdir("packaged plugins",           cfg.package_dir     +os.sep+ 'plugins')
-               cfg.print_configdir("user plugins",               cfg.user_config_dir +os.sep+ 'plugins')
-
-           else:
-               result = cfg.find_conf_file(config)
-               if  not result :
-                   print("no file named %s found in all sarra configs" % config )
-                   sys.exit(1)
-               cfg.list_file(result)
-       else:
-             for d in sorted(cfg.programs):
-                 cfg.print_configdir("configuration examples", cfg.package_dir     +os.sep+ 'examples' +os.sep+ d)
-             cfg.print_configdir("general",                    cfg.user_config_dir )
-             for d in sorted(cfg.programs):
-                 cfg.print_configdir("user configurations",    cfg.user_config_dir + os.sep + d)
-       sys.exit(0)
-
-    # Init logger here
-    cfg.build_instance(1)
-    cfg.setlog()
-
-    # loop on all possible programs ... add audit
-    programs = ['audit']
-    programs.extend(cfg.programs)
-    for pgm in programs:
-        scandir(cfg.user_config_dir, pgm, action)
-
-    sys.exit(0)
 
 # =========================================
 # direct invocation
 # =========================================
 
-if __name__=="__main__":
-   main()
+if __name__ == "__main__":
+    main()
---
+++ test/unit_tests/sr_retry_unittest.py
@@ -0,0 +1,29 @@
+import unittest
+from sarra.sr_retry import sr_retry
+
+
+class TestSrRetry(unittest.TestCase):
+    def setUp(self):
+        self.sr_retry = sr_retry()
+
+    def tearDown(self):
+        self.widget.dispose()
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
